2b5d2ea6fca9026ff82215b49b35a1af6ef21846
Daniel Precioso <danipg1409@gmail.com>
2020-12-02 19:57:19 +0100

    Docstring

--- snapy/astrodynamics.py
+++ snapy/astrodynamics.py
@@ -1,76 +1,169 @@
+from math import sin, cos, acos
+
 import numpy as np
-from math import (sin, cos, acos)
 
 
-def direction_cosine_matrix(thetas: np.array):
+def direction_cosine_matrix(thetas: np.array) -> np.array:
+    """The Direction Cosine Matrix (DCM) is a 3 by 3 matrix that defines the rotations
+    between two reference frames.
+
+    Parameters
+    ----------
+    thetas : numpy.array
+        The Euler rotation angles (roll, pitch, yaw), in radians
+
+    Returns
+    -------
+    c : numpy.array
+        Direction Cosine Matrix
+
     """
-    The Direction Cosine Matrix (DCM) is a 3 by 3 matrix
-    that defines the rotations between two reference frames.
-    theta are the Euler rotation angles (roll, pitch, yaw)
-    """
-    r1 = np.array([[1, 0, 0],
-                    [0, cos(thetas[0]), sin(thetas[0])],
-                    [0, -sin(thetas[0]), cos(thetas[0])]])
-    r2 = np.array([[cos(thetas[1]), 0, -sin(thetas[1])],
-                    [0, 1, 0],
-                    [sin(thetas[1]), 0, cos(thetas[1])]])
-    r3 = np.array([[cos(thetas[2]), sin(thetas[2]), 0],
-                    [-sin(thetas[2]), cos(thetas[2]), 0],
-                    [0, 0, 1]])
+    r1 = np.array(
+        [
+            [1, 0, 0],
+            [0, cos(thetas[0]), sin(thetas[0])],
+            [0, -sin(thetas[0]), cos(thetas[0])],
+        ]
+    )
+    r2 = np.array(
+        [
+            [cos(thetas[1]), 0, -sin(thetas[1])],
+            [0, 1, 0],
+            [sin(thetas[1]), 0, cos(thetas[1])],
+        ]
+    )
+    r3 = np.array(
+        [
+            [cos(thetas[2]), sin(thetas[2]), 0],
+            [-sin(thetas[2]), cos(thetas[2]), 0],
+            [0, 0, 1],
+        ]
+    )
     c = np.dot(np.dot(r1, r2), r3)
     return c
 
 
-def rotate_frame(x: np.array, thetas: np.array):
-    """
-    Rotate to one reference frame to the other
+def rotate_frame(x: np.array, thetas: np.array) -> np.array:
+    """Rotate to one reference frame to the other
+
+    Parameters
+    ----------
+    x : numpy.array
+        Reference frame
+    thetas : numpy.array
+        The Euler rotation angles (roll, pitch, yaw), in radians
+
+    Returns
+    -------
+    x_c : numpy.array
+        The new reference frame, rotated according to the Euler angles
+
     """
     c = direction_cosine_matrix(thetas)
     x_c = np.dot(c, x)
     return x_c
-    
 
-def eigen_axis_rotation(c: np.array):
-    assert c.shape == (3, 3)
-    theta = acos(0.5 * (c[0,0] + c[1,1] + c[2,2] - 1))
+
+def eigen_axis_rotation(c: np.array) -> float:
+    """Angle of DCM
+
+    Parameters
+    ----------
+    c : numpy.array
+        Direction Cosine Matrix
+
+    Returns
+    -------
+    theta : float
+        Angle in radians
+
+    """
+    try:
+        theta = acos(0.5 * (c[0, 0] + c[1, 1] + c[2, 2] - 1))
+    except IndexError:
+        raise IndexError(
+            f"The shape of DCM matrix must be (3,3). Given DCM was " f"{c.shape}"
+        )
+
     return theta
 
 
-def quaternions(e: np.array, theta: float):
-    """
-    Quaternion elements do not carry a direct intuitive meaning.
-    The Quaternion representation however simplifies the kinematic
-    and dynamic equations and does not suffer from singularities
-    which do occur in Euler angle representations.
+def quaternions(e: np.array, theta: float) -> tuple:
+    """Quaternion elements do not carry a direct intuitive meaning.
+    The Quaternion representation however simplifies the kinematic and dynamic
+    equations and does not suffer from singularities which do occur in Euler angle
+    representations.
+
+    Parameters
+    ----------
+    e : numpy.array
+    theta : float
+
+    Returns
+    -------
+    q : numpy.array
+    q4 : float
+
     """
     q = e * sin(theta / 2)
     q4 = cos(theta / 2)
     return q, q4
 
 
-def matrix_angular_rotation(w: np.array):
-    omega = np.array([[0, -w[2], w[1]],
-                     [w[2], 0, -w[0]],
-                     [-w[1], w[0], 0]])
+def matrix_angular_rotation(w: np.array) -> np.array:
+    """
+
+    Parameters
+    ----------
+    w : numpy.array
+
+    Returns
+    -------
+    omega : numpy.array
+
+    """
+    try:
+        omega = np.array([[0, -w[2], w[1]], [w[2], 0, -w[0]], [-w[1], w[0], 0]])
+    except IndexError:
+        raise IndexError(f"w vector must have length 3. Given w is length {len(w)}")
     return omega
 
 
-def quartenions_dot(w: np.array, q: np.array, q4: float):
-    q_dot = .5 * (q4 * w - np.cross(w, q))
-    q4_dot = -.5 * np.dot(np.transpose(w), q)
+def quartenions_dot(w: np.array, q: np.array, q4: float) -> tuple:
+    """
+
+    Parameters
+    ----------
+    w : numpy.array
+    q : numpy.array
+    q4 : float
+
+    Returns
+    -------
+    q_dot : numpy.array
+    q4_dot : float
+
+    """
+    q_dot = 0.5 * (q4 * w - np.cross(w, q))
+    q4_dot = -0.5 * np.dot(np.transpose(w), q)
     return q_dot, q4_dot
 
 
-def dynamic_equation(J: np.array, w_dot: np.array, w: np.array):
+def dynamic_equation(j: np.array, w_dot: np.array, w: np.array):
     """
+
     Parameters
     ----------
-    J : np.array
+    j : numpy.array
         Body's interia matrix
+    w_dot : numpy.array
+    w : numpy.array
+
     Returns
     -------
-    M : np.array
+    m : np.array
         External angular moment applied to the body's main axes
+
     """
-    M = np.dot(J, w_dot) + np.cross(w_dot, np.dot(J, w))
-    return M
+    m = np.dot(j, w_dot) + np.cross(w_dot, np.dot(J, w))
+    return m
--- snapy/gravity.py
+++ snapy/gravity.py
@@ -2,58 +2,88 @@ import numpy as np
 
 from snapy.astrodynamics import rotate_frame
 
-
 M_EARTH = 5.972 * 1e24  # kg
 G = 6.67408 * 1e-11  # m3 kg-1 s-2
 MU = 3.896 * 1e14  # m3 s−2
 
 
-def gravity_acceleration(x: np.array):
-    """
+def gravity_acceleration(x: np.array, m: float = M_EARTH) -> np.array:
+    """Acceleration due to some mass' gravity at a certain point in space
+    By default the mass is the Earth, so coordinates are given in ECI
+
     Parameters
     ----------
-    x : np.array
-        In ECI
+    x : numpy.array
+        Position relative to the mass, in meters
+    m : float, optional
+        Mass of the body producing the gravity, in kilograms. By default 5.972 * 1e24 kg
+
+    Returns
+    -------
+    a : numpy.array
+        Acceleration relative to the mass, in meters per seconds squared
+
     """
-    a = G * M_EARTH / np.dot(x, x)
+    a = G * m / np.dot(x, x)
     return a
 
 
-def nadir_vector(x: np.array):
+def nadir_vector(x: np.array) -> np.array:
     """
+
     Parameters
     ----------
-    x : np.array
-        In ECI
+    x : numpy.array
+        Position relative to ECI, in meters
+
+    Returns
+    -------
+    u_e : numpy.array
+        Nadir vector
+
     """
-    u_e = - x / np.linalg.norm(x)
+    u_e = -x / np.linalg.norm(x)
     return u_e
 
 
-def gravitational_force(x: np.array, m_sat: float):
+def gravitational_force(x: np.array, m_sat: float, m_body: float = M_EARTH) -> np.array:
     """
+
     Parameters
     ----------
-    x : np.array
-        In ECI
+    x : numpy.array
+        Position relative to gravity pull, in meters
+    m_sat : float
+        Satellite's mass, in kilograms
+    m_body : float, optional
+        Mass of the body producing the gravity, in kilograms. By default 5.972 * 1e24 kg
+
     Returns
     -------
     f_g : np.array
-        Gravitational force (N)
+        Gravitational force, in Newtons
+
     """
-    a = gravity_acceleration(x)
+    a = gravity_acceleration(x, m=m_body)
     nadir = nadir_vector(x)
     f_g = a * nadir * m_sat
     return f_g
 
 
-def gravity_gradient(x: np.array, thetas: np.array,
-                     inertia: np.array):
+def gravity_gradient(x: np.array, thetas: np.array, inertia: np.array) -> np.array:
     """
+
+    Parameters
+    ----------
+    x : numpy.array
+    thetas : numpy.array
+    inertia : numpy.array
+
     Returns
     -------
-    m_gg : np.array
-        Gravity gradient torque, in bodyframe (N m)
+    m_gg : numpy.array
+        Gravity gradient torque relative to bodyframe, in Newtons * meters
+
     """
     # Distance to the center (ECI)
     r = np.linalg.norm(x)
@@ -62,22 +92,33 @@ def gravity_gradient(x: np.array, thetas: np.array,
     # Nadir vector (body)
     u_e = nadir_vector(x_body)
     # Gravity gradient torque
-    m_gg = (3 * MU / r**3) * np.cross(u_e, np.dot(inertia, u_e))
+    m_gg = (3 * MU / r ** 3) * np.cross(u_e, np.dot(inertia, u_e))
     return m_gg
 
 
-def gravity_gradient_smart(x: np.array, thetas: np.array,
-                           inertia: np.array):
-    """
-    Same as gravity_gradient but reduces
-    the number of computations
+def gravity_gradient_smart(
+    x: np.array, thetas: np.array, inertia: np.array
+) -> np.array:
+    """Same as gravity_gradient but reduces the number of computations
+
+    Parameters
+    ----------
+    x : numpy.array
+    thetas : numpy.array
+    inertia : numpy.array
+
+    Returns
+    -------
+    m_gg : numpy.array
+        Gravity gradient torque relative to bodyframe, in Newtons * meters
+
     """
     # Bodyframe position
     x_body = rotate_frame(x, thetas)
     # Nadir vector (body)
     u_e = nadir_vector(x_body)
     # Gravity gradient torque
-    m_gg = ((3 * G * M_EARTH / (np.dot(x, x)**2))
-            * np.cross(-x_body, np.dot(inertia, u_e)))
+    m_gg = (3 * G * M_EARTH / (np.dot(x, x) ** 2)) * np.cross(
+        -x_body, np.dot(inertia, u_e)
+    )
     return m_gg
-    
