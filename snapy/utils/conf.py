eade49a87c26590cdf98ff348e4e3238136a7b5c
Daniel Precioso <danipg1409@gmail.com>
2020-12-25 13:45:37 +0100

    Config lists are loaded as numpy arrays

--- snapy/utils/conf.py
+++ snapy/utils/conf.py
@@ -2,6 +2,7 @@ import warnings
 from pathlib import Path
 from typing import Union
 
+import numpy as np
 import toml
 
 
@@ -29,21 +30,28 @@ class Conf(dict):
             val = None
         else:
             val = dict.__getitem__(self, key)
+
+        # Transform list to numpy
+        if type(val) is list:
+            val = np.array(val)
         return val
 
 
 def load_conf(path: Union[str, Path], key: str = None) -> Conf:
     """Load TOML config as dict-like
+
     Parameters
     ----------
     path : str
         Path to TOML config file
     key : str, optional
         Section of the conf file to load
+
     Returns
     -------
     Conf
         Config dictionary
+
     """
     config = toml.load(path)
     return Conf(config) if key is None else Conf(config[key])
